1825c1bbce53abe47411a70b1f57505fe473ccc9
andAndrew <andAndrew.k@gmail.com>
2022-11-25 14:55:38 +0400

    feat: added '404: PAGE NOT FOUND' route

--- src/app/AppWithRedux.tsx
+++ src/app/AppWithRedux.tsx
@@ -75,6 +75,8 @@ function AppWithRedux() {
                                     </Grid></>
                                 : <Navigate to={'/login'}/>}/>
                         <Route path={'/login'} element={<Login/>}/>
+                        <Route path={'/404'} element={<h1>404: PAGE NOT FOUND</h1>}/>
+                        <Route path={'*'} element={<Navigate to={'/404'}/>}/>
                     </Routes>
                 </Container>
             </div>
